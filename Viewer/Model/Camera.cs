90f72dc353c2d6ef80da899a39c4812263245c9b
Anton Dubovik <antonimxia@gmail.com>
2024-10-15 22:14:19 +0300

    добавление ортогональной проекции

--- Viewer/Model/Camera.cs
+++ Viewer/Model/Camera.cs
@@ -9,11 +9,11 @@ namespace Viewer.Model
         public float AngleY { get; private set; }
         public float Distance { get; private set; }
 
-        private const float MIN_DISTANCE = 2f; 
-        private const float MAX_DISTANCE = 15; 
+        private const float MIN_DISTANCE = 2f;
+        private const float MAX_DISTANCE = 15;
 
-        private const float MIN_ANGLE_X= -1.5f;
-        private const float MAX_ANGLE_X= 1.5f;
+        private const float MIN_ANGLE_X = -1.5f;
+        private const float MAX_ANGLE_X = 1.5f;
 
         private const float MIN_DZ = 0.01f;
 
@@ -37,35 +37,51 @@ namespace Viewer.Model
             AngleX = Math.Max(MIN_ANGLE_X, Math.Min(MAX_ANGLE_X, AngleX - deltaY * 0.01f));
         }
 
-        // проекция 3D в 2D
-        public PointF Project(float x, float y, float z, Size clientSize)
+        
+        public PointF Project(float x, float y, float z, Size clientSize, bool isOrthogonal = false)
         {
             float cosX = (float)Math.Cos(AngleX);
             float sinX = (float)Math.Sin(AngleX);
             float cosY = (float)Math.Cos(AngleY);
             float sinY = (float)Math.Sin(AngleY);
 
+           
             float dx = x * cosY - z * sinY;
             float dz = x * sinY + z * cosY;
             float dy = y * cosX - dz * sinX;
             dz = y * sinX + dz * cosX;
 
+            return isOrthogonal ? OrthogonalProjection(dx, dy, clientSize) : PerspectiveProjection(dx, dy, dz, clientSize);
+        }
+
+        private PointF OrthogonalProjection(float dx, float dy, Size clientSize)
+        {
+            float factor = 500 / Distance;
+            float projectedX = dx * factor + clientSize.Width / 2;
+            float projectedY = dy * factor + clientSize.Height / 2;
+
+            return new PointF(projectedX, projectedY);
+        }
+
+        private PointF PerspectiveProjection(float dx, float dy, float dz, Size clientSize)
+        {
+            
             float safeDistance = Distance;
 
             if (dz >= safeDistance)
             {
-                safeDistance += 0.1f; 
+                safeDistance += 0.1f;
             }
-
 
             float factor = Distance / (safeDistance - dz);
             float projectedX = dx * factor * 100 + clientSize.Width / 2;
             float projectedY = dy * factor * 100 + clientSize.Height / 2;
 
-            if (float.IsInfinity(projectedX) || float.IsNegativeInfinity(projectedX)) throw new ArgumentException();
+            if (float.IsInfinity(projectedX) || float.IsNegativeInfinity(projectedX))
+                throw new ArgumentException();
 
             return new PointF(projectedX, projectedY);
-
         }
+
     }
 }
